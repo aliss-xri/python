cd527f9d6145f50985361e503c0a4c9d6a21e310
ALISSAN REZVANI <alissanrezvani88@gmail.com>
2025-01-23 14:29:47 +0300

    odd.py file updated

--- odd.py
+++ odd.py
@@ -7,6 +7,6 @@ odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,
 right_this_minute = datetime.today().minute
 
 if right_this_minute in odds:
-    print("this minute seems a little odd.")
+    print("This minute seems a little odd.")
 else:
-    print("not an odd minute.")
+    print("Not an odd minute.")
